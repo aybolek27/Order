9654f74592b0026071a1d635af1c8074bad667e9
Aybolek <aybolek27@gmail.com>
2018-07-01 13:34:27 -0500

    final change

--- src/com/weborder/Order.java
+++ src/com/weborder/Order.java
@@ -1,8 +1,5 @@
 package com.weborder;
-
 import java.util.Random;
-import java.util.Scanner;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,44 +11,44 @@ public class Order {
 				"C:\\Users\\Aybolek\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
-		//navigates to required url
+		// navigates to required url
 		driver.navigate().to("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
-		//enters user name
+		// enters user name
 		driver.findElement(By.name("ctl00$MainContent$username")).sendKeys("Tester");
-		//enters password 
+		// enters password
 		driver.findElement(By.name("ctl00$MainContent$password")).sendKeys("test");
-		//clicks login button
+		// clicks login button
 		driver.findElement(By.name("ctl00$MainContent$login_button")).click();
-		//clicks Order link
+		// clicks Order link
 		driver.findElement(By.linkText("Order")).click();
-		
-		//generates numbers 1 to 100 and enters into quantity textfield
+
+		// generates numbers 1 to 100 and enters into quantity textfield
 		int number = randomGenerator(100);
 		driver.findElement(By.id("ctl00_MainContent_fmwOrder_txtQuantity")).sendKeys(String.valueOf(number));
-		
-		//String array of middle names in each run uses differnt one
+
+		// String array of middle names in each run uses differnt one
 		String[] midNames = { "Blake", "Dean", "Grant", "Hugh", "James", "Charles", "George", "Jude", "Rhett", "Kent",
 				"Lee", "Brett", "Luke", "Chase", "Claude", "Paul", "Reese", "Sean", "Trey", "Bram" };
-		//generates random index
+		// generates random index
 		int index = randomGenerator(midNames.length);
-		//send name + middleName + lastName
+		// send name + middleName + lastName
 		driver.findElement(By.name("ctl00$MainContent$fmwOrder$txtName"))
 				.sendKeys("John " + midNames[index] + " Smith");
-		//enteres street
+		// enteres street
 		driver.findElement(By.name("ctl00$MainContent$fmwOrder$TextBox2")).sendKeys("123 Any st");
-		//enteres city
+		// enteres city
 		driver.findElement(By.name("ctl00$MainContent$fmwOrder$TextBox3")).sendKeys("Alexandria");
-		//enters state as required
+		// enters state as required
 		driver.findElement(By.name("ctl00$MainContent$fmwOrder$TextBox4")).sendKeys("Virginia");
-		//generates 5 digit zipcode and enters into requierd field
+		// generates 5 digit zipcode and enters into requierd field
 		String zip = "";
 		for (int i = 0; i < 5; i++) {
 			zip += randomGenerator(9);
 		}
 		driver.findElement(By.name("ctl00$MainContent$fmwOrder$TextBox5")).sendKeys(zip);
-		//chooses one of card type
-		//in each run different card type selected
-		//and card number generated using numbergenerator method
+		// chooses one of card type
+		// in each run different card type selected
+		// and card number generated using numbergenerator method
 		int choice = randomGenerator(3);
 		if (choice == 1) {
 			driver.findElement(By.id("ctl00_MainContent_fmwOrder_cardList_0")).click();
@@ -67,10 +64,10 @@ public class Order {
 			String cardNumber = "3" + cardNumberGenerator(14);
 			driver.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox6")).sendKeys(cardNumber);
 		}
-		//provided valid expired date
+		// provided valid expired date
 		driver.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox1")).sendKeys("02/18");
 		driver.findElement(By.id("ctl00_MainContent_fmwOrder_InsertButton")).click();
-		//Verified that the page contains text New order has been successfully added.
+		// Verified that the page contains text New order has been successfully added.
 		if (driver.getPageSource().contains("New order has been successfully added.")) {
 			System.out.println("Text present");
 		} else {
@@ -78,13 +75,14 @@ public class Order {
 		}
 
 	}
-
-	public static int randomGenerator(int limit) {
+		//generates random numbers in given range
+	public static int randomGenerator(int range) {
 		Random r = new Random();
-		return r.nextInt(limit) + 1;
+		return r.nextInt(range) + 1;
 
 	}
-
+	
+	//generates card numbers in given length
 	public static String cardNumberGenerator(int length) {
 		String cardNumber = "";
 		for (int i = 0; i < length; i++) {
